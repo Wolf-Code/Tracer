56cd41750e3af3f9c9e8046e660d55f2606f66dd
xThaWolfx <kevin.deelen@gmail.com>
2015-01-03 00:53:00 +0100

    OpenCL start
    Not sure if this will be added in the long run, but it's not a bad thing
    to start.

---
+++ Tracer/Renderers/OpenCLRenderer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenCL.Net;
+using Tracer.Classes.Objects;
+
+namespace Tracer.Renderers
+{
+    internal class OpenCLRenderer : IRenderer
+    {
+        public event EventHandler<RendererProgressEventArgs> OnProgress;
+        public event EventHandler<RendererFinishedEventArgs> OnFinished;
+
+        private Device _device;
+        private Context _context;
+
+        private void CheckErr( ErrorCode err, string name )
+        {
+            if ( err != ErrorCode.Success )
+            {
+                Console.WriteLine( "ERROR: " + name + " (" + err.ToString( ) + ")" );
+            }
+        }
+
+        private void ContextNotify( string errInfo, byte [ ] data, IntPtr cb, IntPtr userData )
+        {
+            Console.WriteLine( "OpenCL Notification: " + errInfo );
+        }
+
+        private void Setup( )
+        {
+            ErrorCode error;
+            Platform [ ] platforms = Cl.GetPlatformIDs( out error );
+            List<Device> devicesList = new List<Device>( );
+
+            CheckErr( error, "Cl.GetPlatformIDs" );
+
+            foreach ( Platform platform in platforms )
+            {
+                string platformName = Cl.GetPlatformInfo( platform, PlatformInfo.Name, out error ).ToString( );
+                Console.WriteLine( "Platform: " + platformName );
+                CheckErr( error, "Cl.GetPlatformInfo" );
+                //We will be looking only for GPU devices
+                foreach ( Device device in Cl.GetDeviceIDs( platform, DeviceType.Gpu, out error ) )
+                {
+                    CheckErr( error, "Cl.GetDeviceIDs" );
+                    Console.WriteLine( "Device: " + Cl.GetDeviceInfo( device, DeviceInfo.Name, out error ) );
+                    CheckErr( error, "Cl.GetDeviceInfo" );
+                    devicesList.Add( device );
+                }
+            }
+
+            if ( devicesList.Count <= 0 )
+            {
+                Console.WriteLine( "No devices found." );
+                return;
+            }
+
+            _device = devicesList[ 0 ];
+
+            if ( Cl.GetDeviceInfo( _device, DeviceInfo.ImageSupport,
+                out error ).CastTo<Bool>( ) == Bool.False )
+            {
+                Console.WriteLine( "No image support." );
+                return;
+            }
+            _context
+                = Cl.CreateContext( null, 1, new [ ] { _device }, ContextNotify,
+                    IntPtr.Zero, out error ); //Second parameter is amount of devices
+            CheckErr( error, "Cl.CreateContext" );
+        }
+
+        public void Run( )
+        {
+            Setup(  );
+        }
+
+        public void Cancel( )
+        {
+        }
+
+        public void RenderImage( Scene Scn, uint Samples, uint Depth )
+        {
+        }
+    }
+}
